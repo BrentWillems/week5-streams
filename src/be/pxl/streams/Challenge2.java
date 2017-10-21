83dee4bf1d43fed0ec3f2228c7b1a85b7b655df6
BrentWillems <brent.willems.bw@gmail.com>
2017-10-21 14:47:06 +0200

    Finished ex 1 & 2

--- src/be/pxl/streams/Challenge2.java
+++ src/be/pxl/streams/Challenge2.java
@@ -1,14 +1,22 @@
 package be.pxl.streams;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Challenge2 {
 	public static void main(String[] args) {
 		// maak een stream met 10 random gehele getallen tussen 0 en 30
 		// bekijk hiervoor de methode ints(...) in de klasse Random
+		List<Integer> array = new Random().ints(0,30).limit(10).boxed().collect(Collectors.toList());
 		// filter de getallen die deelbaar zijn door 3
+		array.stream().filter( n -> n%3==0).forEach(System.out::println);
 		// en geef het maximum 
+		System.out.println("/*-/*-/*-");
+		array.stream().sorted().reduce((p1,p2) -> p2).ifPresent(System.out::println);
 		// gebruik eventueel peek() om een tussenresultaat van de stream te tonen
-		
-		
-		
 	}
 }
