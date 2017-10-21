e1194aac035cfff86dc88a22aeeea022496c0d15
BrentWillems <brent.willems.bw@gmail.com>
2017-10-21 14:53:14 +0200

    Finished ex 3.1

--- src/be/pxl/streams/Challenge3.java
+++ src/be/pxl/streams/Challenge3.java
@@ -3,6 +3,8 @@ package be.pxl.streams;
 import java.util.Arrays;
 import java.util.List;
 
+import com.sun.xml.internal.ws.util.StringUtils;
+
 public class Challenge3 {
 	public static void main(String[] args) {
 		List<String> topNames = Arrays.asList(
@@ -19,6 +21,8 @@ public class Challenge3 {
         );
 		// 1. Druk alle namen van de lijst af in gesorteerde volgorde.
 		// Zorg dat de eerste letter van een naam steeds met hoofdletter begint.
+		
+		topNames.stream().map(n -> StringUtils.capitalize(n)).sorted().forEach(System.out::println);
 		
 		
 		// 2. Druk alle namen af die beginnen met A (of a)
